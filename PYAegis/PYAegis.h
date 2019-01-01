884b82ef79cab2aca4445062ecdc372e644fd585
Pluto-Y <kuaileainits@163.com>
2019-01-01 21:34:01 +0800

    Initiliaze the new project

---
+++ PYAegis/PYAegis.h
@@ -0,0 +1,19 @@
+//
+//  PYAegis.h
+//  PYAegis
+//
+//  Created by Longhua Zhang on 2019/1/1.
+//  Copyright © 2019 Pluto-Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PYAegis.
+FOUNDATION_EXPORT double PYAegisVersionNumber;
+
+//! Project version string for PYAegis.
+FOUNDATION_EXPORT const unsigned char PYAegisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PYAegis/PublicHeader.h>
+
+
